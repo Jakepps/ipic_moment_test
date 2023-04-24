24abf484eabdb0e37e9de0a3aec78bbf60f920a0
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-04-24 13:24:37 +0300

    Format code style mozilla

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -2,11 +2,9 @@
 #include <cstdlib>
 
 // mega cringe code, aaaaahahahahahahah, sup (это я придумал а серега украл ха-ха)
-// new comment
 int main()
 {
-    std::string name;
-    std::cout << "Please enter your name: ";
+    std::string name; std::cout << "Please enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
