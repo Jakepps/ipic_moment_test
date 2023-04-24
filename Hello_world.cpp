d3cb08f59cfab61ee6530d789e72eb66ba65ede1
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-04-24 13:42:15 +0300

    Update Hello_world.cpp

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -2,10 +2,11 @@
 #include <cstdlib>
 
 // mega cringe code, aaaaahahahahahahah, sup (это я придумал а серега украл ха-ха)
+// new comment fdf
 int main()
 {
-    std::string name; std::cout << "Please enter your name: ";
+    std::string name;
+    std::cout << "Please enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
-}
